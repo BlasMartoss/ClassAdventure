563a9eebe25978c581dc89813e623bac6bbe1e03
Blas Martos <bmartosortega@gmail.com>
2024-05-22 17:10:48 +0200

    Added Select Role Page

--- App.js
+++ App.js
@@ -3,15 +3,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import Signin from './Pages/Signin';
 import Signup from './Pages/Signup';
+import SelectRole from './Pages/SelectRole';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Signin">
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="SelectRole">
+        
         <Stack.Screen name="Signin" component={Signin} />
         <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="SelectRole" component={SelectRole} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Pages/SelectRole.js
@@ -0,0 +1,177 @@
+import { StatusBar } from 'expo-status-bar';
+import * as React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image, Modal } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react';
+
+export default function Register({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedRole, setSelectedRole] = useState('');
+
+  const handlePress = (role) => {
+    setSelectedRole(role);
+    setModalVisible(true);
+  };
+
+  const handleConfirm = (response) => {
+    if (response === 'yes') {
+      // Maneja la confirmación aquí
+    }
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Select Role</Text>
+      <Text style={styles.subTitle}>This will be the role in your new account.</Text>
+      <View style={styles.imagesView}>
+        <View style={styles.containerView}>
+          <Image
+            source={require('../images/Participant.png')}
+            style={styles.image}
+          />
+          <TouchableOpacity style={styles.button} onPress={() => handlePress('a Participant')}>
+            <LinearGradient
+              colors={['#89BBE9', '#3E9CF3']}
+              style={styles.linearGradient}
+            >
+              <Text style={styles.buttonText}>Participant</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.containerView}>
+          <Image
+            source={require('../images/Organizer.png')}
+            style={styles.image}
+          />
+          <TouchableOpacity style={styles.button} onPress={() => handlePress('an Organizer')}>
+            <LinearGradient
+              colors={['#89BBE9', '#3E9CF3']}
+              style={styles.linearGradient}
+            >
+              <Text style={styles.buttonText}>Organizer</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalText}>Are you sure you want to be {selectedRole}?</Text>
+            <View style={styles.modalButtons}>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => handleConfirm('yes')}
+              >
+                <Text style={styles.modalButtonText}>YES</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => handleConfirm('no')}
+              >
+                <Text style={styles.modalButtonText}>NO</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerView: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 150,
+    margin: '10%',
+    marginTop: '2%',
+    marginBottom: '5%',
+  },
+  imagesView: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  buttonView: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#344340',
+  },
+  subTitle: {
+    fontSize: 15,
+    color: 'gray',
+    paddingBottom: '5%',
+  },
+  button: {
+    width: '80%',
+    borderRadius: 30,
+    marginBottom: '20%',
+    margin: '2%',
+  },
+  linearGradient: {
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  modalBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContainer: {
+    width: 300,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  modalText: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  modalButton: {
+    flex: 1,
+    padding: 10,
+    marginHorizontal: 5,
+    backgroundColor: '#3E9CF3',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  modalButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});

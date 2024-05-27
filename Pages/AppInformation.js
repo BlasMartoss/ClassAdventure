04786192147a688c726754eb5e9e22203fe096b5
SilverMoonDev <jcarcelle03@gmail.com>
2024-05-27 19:01:22 +0200

    Made a Component For the back button

--- Pages/AppInformation.js
+++ Pages/AppInformation.js
@@ -1,39 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image, Modal } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, Text, View, Image } from "react-native";
+import BackButton from "../components/BackButton";
 
 export default function Home({ navigation }) {
-
-
   return (
     <View style={styles.container}>
-            <View style={styles.HeaderContainer}>
-                    <View style={styles.upgradeContainer} >
-                        <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('ProfileSettingsParticipant')}>
-                            <Text style={styles.backText}>&lt;</Text>
-                        </TouchableOpacity>
-                    </View>
-            </View>
-        <View style={styles.viewOptions}>
-            <Text style={styles.userProfileText}>App Information</Text>
-            <Image
-                source={require('../images/AppInformationIcon.png')}
-                style={styles.icon}
-            />
-        </View>
+      <BackButton navigation={navigation} />
+      <View style={styles.viewOptions}>
+        <Text style={styles.userProfileText}>App Information</Text>
+        <Image
+          source={require("../images/AppInformationIcon.png")}
+          style={styles.icon}
+        />
+      </View>
 
-        <View style={styles.infoContainer}>
-            <Text style={styles.userProfileText}>Updates</Text>
-            <Image
-                source={require('../images/AppInformationIcon.png')}
-                style={styles.iconInfo}
-            />
-        </View>
-        <View style={styles.line}></View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.userProfileText}>Updates</Text>
+        <Image
+          source={require("../images/AppInformationIcon.png")}
+          style={styles.iconInfo}
+        />
+      </View>
+      <View style={styles.line}></View>
 
-
-      
       <StatusBar style="auto" />
     </View>
   );
@@ -41,98 +31,62 @@ export default function Home({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f1f1f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: '10%'
+    backgroundColor: "#f1f1f1",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "10%",
   },
-     upgradeContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        textAlign: 'center',
-        alignItems: 'center',
-        width: '80%',
-        padding: '2%',
-        paddingLeft: '5%',
-    },
-       // Back Button Style
-       HeaderContainer: {
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'row',
-        width: '100%',
-        alignContent: 'center',
-    },
-    backText: {
-        fontSize: 18,
-        padding: '2%',
-        paddingRight: '5%',
-        paddingLeft: "5%",
-    },
-    backButton: {
-        backgroundColor: 'white',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    // end back button
   viewOptions: {
-    width: '100%',
-    marginBottom: '5%',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-
+    width: "100%",
+    marginBottom: "5%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 
   userProfileText: {
     fontSize: 20,
-    marginLeft: '5%',
-
+    marginLeft: "5%",
   },
   icon: {
     height: 40,
-    width: 40, 
-    marginRight: '5%' 
+    width: 40,
+    marginRight: "5%",
   },
   iconInfo: {
     height: 40,
-    width: 40, 
-    marginRight: '10%' 
+    width: 40,
+    marginRight: "10%",
   },
   infoContainer: {
-    marginTop: '10%',
-    width: '100%',
-    marginBottom: '2%',
-    display: 'flex',
-    justifyContent:'space-between',
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignContent: 'center',
-    paddingRight: '10%'
+    marginTop: "10%",
+    width: "100%",
+    marginBottom: "2%",
+    display: "flex",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center",
+    paddingRight: "10%",
   },
   line: {
     width: "80%",
-    marginTop: '1%',
-    marginRight: '20%',
-    marginLeft: '5%',
+    marginTop: "1%",
+    marginRight: "20%",
+    marginLeft: "5%",
     height: 0.8,
-    marginBottom:'4%',
-    backgroundColor: 'black'
+    marginBottom: "4%",
+    backgroundColor: "black",
   },
   role: {
-    color: '#888',
+    color: "#888",
     fontSize: 18,
-    textAlign: 'center',
+    textAlign: "center",
   },
   username: {
-    color: 'black',
+    color: "black",
     fontSize: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
-
 });
--- Pages/ProfileSettingsOrganizer.js
+++ Pages/ProfileSettingsOrganizer.js
@@ -1,75 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import * as React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, Image, Modal } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import BackButton from "../components/BackButton";
+
 
 export default function Home({ navigation }) {
-
-
   return (
     <View style={styles.container}>
-            <View style={styles.HeaderContainer}>
-                    <View style={styles.upgradeContainer} >
-                        <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('ProfileOrganizer')}>
-                            <Text style={styles.backText}>&lt;</Text>
-                        </TouchableOpacity>
-                    </View>
-            </View>
-        <View style={styles.viewOptions}>
-            <Text style={styles.userProfileText}>Settings</Text>
-           
-        </View>
+      <BackButton navigation={navigation} />
+
+      <View style={styles.viewOptions}>
+        <Text style={styles.userProfileText}>Settings</Text>
+      </View>
+      <Image
+        source={require("../images/iconUser.png")}
+        style={styles.userIcon}
+      />
+      {/*ROLE*/}
+      <Text style={styles.username}>Joan</Text>
+      {/*ROLE*/}
+      <Text style={styles.role}>Organizer</Text>
+      <View style={styles.line}></View>
+
+      <TouchableOpacity
+        style={styles.configurationView}
+        onPress={() => navigation.navigate("")}
+      >
         <Image
-          source={require('../images/iconUser.png')}
-          style={styles.userIcon}
+          source={require("../images/EditProfileIcon.png")}
+          style={styles.icon}
         />
-        {/*ROLE*/}
-        <Text style={styles.username}>Joan</Text>
-        {/*ROLE*/}
-        <Text style={styles.role}>Organizer</Text>
-        <View style={styles.line}></View>
-    
-        <TouchableOpacity style={styles.configurationView} onPress={() => navigation.navigate('')}> 
-            <Image
-                source={require('../images/EditProfileIcon.png')}
-                style={styles.icon}
-            />
-            <Text style={styles.text}>Edit Profile</Text>
-        </TouchableOpacity>
+        <Text style={styles.text}>Edit Profile</Text>
+      </TouchableOpacity>
 
-       <TouchableOpacity style={styles.configurationView} onPress={() => navigation.navigate('')}> 
-            <Image
-                source={require('../images/ChangePasswordIcon.png')}
-                style={styles.icon}
-            />
-            <Text style={styles.text}>Change Password</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.configurationView}
+        onPress={() => navigation.navigate("")}
+      >
+        <Image
+          source={require("../images/ChangePasswordIcon.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.text}>Change Password</Text>
+      </TouchableOpacity>
 
-       <TouchableOpacity style={styles.configurationView} onPress={() => navigation.navigate('UpgradePlan')}> 
-            <Image
-                source={require('../images/Diamond Premium.png')}
-                style={styles.icon}
-            />
-            <Text style={styles.text}>Update Plan</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.configurationView}
+        onPress={() => navigation.navigate("UpgradePlan")}
+      >
+        <Image
+          source={require("../images/Diamond Premium.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.text}>Update Plan</Text>
+      </TouchableOpacity>
 
-        <TouchableOpacity style={styles.configurationView} onPress={() => navigation.navigate('UpgradePlan')}> 
-            <Image
-                source={require('../images/AppInformationIcon.png')}
-                style={styles.icon}
-            />
-            <Text style={styles.text}>App Information</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.configurationView}
+        onPress={() => navigation.navigate("UpgradePlan")}
+      >
+        <Image
+          source={require("../images/AppInformationIcon.png")}
+          style={styles.icon}
+        />
+        <Text style={styles.text}>App Information</Text>
+      </TouchableOpacity>
 
-            <TouchableOpacity style={styles.logoutView} onPress={() => navigation.navigate('')}> 
-                <Image  
-                    source={require('../images/LogoutIcon.png')}
-                    style={styles.iconlogout}
-                />
-                <Text style={styles.text}>Log Out</Text>
-             </TouchableOpacity>
-   
-      
+      <TouchableOpacity
+        style={styles.logoutView}
+        onPress={() => navigation.navigate("")}
+      >
+        <Image
+          source={require("../images/LogoutIcon.png")}
+          style={styles.iconlogout}
+        />
+        <Text style={styles.text}>Log Out</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -77,102 +84,69 @@ export default function Home({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#f1f1f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: '10%'
+    backgroundColor: "#f1f1f1",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: "10%",
   },
-     upgradeContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        textAlign: 'center',
-        alignItems: 'center',
-        width: '80%',
-        padding: '2%',
-        paddingLeft: '5%',
-    },
-       // Back Button Style
-       HeaderContainer: {
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'row',
-        width: '100%',
-        alignContent: 'center',
-    },
-    backText: {
-        fontSize: 18,
-        padding: '2%',
-        paddingRight: '5%',
-        paddingLeft: "5%",
-    },
-    backButton: {
-        backgroundColor: 'white',
-        borderRadius: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    // end back button
   viewOptions: {
-    width: '100%',
-    marginBottom: '5%'
+    width: "100%",
+    marginBottom: "5%",
   },
 
   userProfileText: {
     fontSize: 20,
-    marginLeft: '5%'
+    marginLeft: "5%",
   },
   userIcon: {
     height: 150,
-    width: 150,  
+    width: 150,
   },
   role: {
-    color: '#888',
+    color: "#888",
     fontSize: 18,
-    textAlign: 'center',
+    textAlign: "center",
   },
   username: {
-    color: 'black',
+    color: "black",
     fontSize: 30,
-    textAlign: 'center',
+    textAlign: "center",
   },
   line: {
     width: "90%",
-    marginTop: '5%',
+    marginTop: "5%",
     height: 0.8,
-    marginBottom:'4%',
-    backgroundColor: 'black'
+    marginBottom: "4%",
+    backgroundColor: "black",
   },
   configurationView: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft:'10%',
-    marginTop: '4%',
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: "10%",
+    marginTop: "4%",
   },
   logoutView: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingLeft:'10%',
-    marginTop: '15%',
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingLeft: "10%",
+    marginTop: "15%",
   },
   icon: {
     height: 50,
-    width: 50, 
+    width: 50,
   },
   iconlogout: {
     height: 40,
-    width: 40, 
+    width: 40,
   },
   text: {
-    color: '#888',
+    color: "#888",
     fontSize: 18,
-    textAlign: 'center',
-    marginLeft: '5%',
+    textAlign: "center",
+    marginLeft: "5%",
   },
 });
